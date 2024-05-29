39607b96a6d337afd59087aafef0a6b2821e8c0c
Swapnilkumar Dwivedi <122719184+swappy-2003@users.noreply.github.com>
2024-05-29 20:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var a = document.querySelector("#elem1")
+var b = a.getAttribute("data-image")
+console.log(b)
+console.log("hey hii")
+
+var ab = document.querySelector("#elem-container")
+var g = document.querySelector("#fixed-image")
+ab.addEventListener("mouseenter",function(){
+    g.style.display = "block"
+})
+ab.addEventListener("mouseleave",function(){
+    g.style.display= "none"
+})
+
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        g.style.backgroundImage = `url(${image})`
+    })
+})
